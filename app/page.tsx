a2776df97a7350de741a56fc867f4f04428080a1
CyberM <109589794+Cybernuel@users.noreply.github.com>
2025-07-25 20:50:26 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -78,7 +78,7 @@ export default function Page() {
                       <h3 className="text-xl font-semibold">🚀 My Journey</h3>
                       <div className="space-y-3 text-gray-600 dark:text-gray-300">
                         <p>
-                          I started out in Nigeria, where I earned my B.Eng in Computer Engineering from University of Ilorin, nigeria and quickly immersed
+                          I started out in Nigeria, where I earned my B.Eng in Computer Engineering from University of Ilorin, Nigeria and quickly immersed
                           myself in network security through hands-on internships. At Aero Contractors of Nigeria, I cut my teeth
                           configuring enterprise networks, deploying VPNs, and defending systems against DDoS attacks.
                           That early exposure to real-world cyber threats lit a fire in me, it showed me how critical
